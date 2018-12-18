a7c0687ecc1bcde5a093c7d5b85211a69df4dd61
Avichay Kardash <avichay.kardash@relativity.com>
2018-12-18 13:06:43 +0100

    trying to connect to jira

--- okta-hosted-login/okta-aspnetcore-mvc-example/Controllers/HomeController.cs
+++ okta-hosted-login/okta-aspnetcore-mvc-example/Controllers/HomeController.cs
@@ -1,45 +1,64 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using okta_aspnetcore_mvc_example.Models;
 
 namespace okta_aspnetcore_mvc_example.Controllers
 {
-    public class HomeController : Controller
-    {
-        public IActionResult Index()
-        {
-            return View();
-        }
+	using System;
+	using System.Net;
+	using Newtonsoft.Json;
 
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
+	public class HomeController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
 
-            return View();
-        }
+		public IActionResult About()
+		{
+			ViewData["Message"] = "Your application description page.";
 
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
+			return View();
+		}
 
-            return View();
-        }
+		public IActionResult Contact()
+		{
+			ViewData["Message"] = "Your contact page.";
 
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+			return View();
+		}
+
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
 
 
-        [Authorize]
-        public IActionResult Profile()
-        {
-            return View(HttpContext.User.Claims);
-        }
-    }
+		[Authorize]
+		public IActionResult Profile()
+		{
+			return View(HttpContext.User.Claims);
+		}
+
+		public IActionResult Jira()
+		{
+			const string uri = "https://jira.kcura.com/rest/api/latest/search?jql=assignee=currentUser()&fields=id,key";
+
+			using (var w = new WebClient())
+			{
+				string jsonData = string.Empty;
+
+				w.Headers.Add("Content-Type", "application/json");
+				jsonData = w.DownloadString(uri);
+
+
+				
+				//return !string.IsNullOrEmpty(jsonData) ? JsonConvert.DeserializeObject<T>(jsonData) : new T();
+			}
+
+			return View();
+		}
+	}
 }
--- okta-hosted-login/okta-aspnetcore-mvc-example/Startup.cs
+++ okta-hosted-login/okta-aspnetcore-mvc-example/Startup.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using Microsoft.AspNetCore.Authentication.Cookies;
-using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -12,57 +8,55 @@ using Okta.AspNetCore;
 
 namespace okta_aspnetcore_mvc_example
 {
-    public class Startup
-    {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
+	public class Startup
+	{
+		public Startup(IConfiguration configuration)
+		{
+			Configuration = configuration;
+		}
 
-        public IConfiguration Configuration { get; }
+		public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            var oktaMvcOptions = new OktaMvcOptions();
-            Configuration.GetSection("Okta").Bind(oktaMvcOptions);
-            oktaMvcOptions.Scope = new List<string> { "openid", "profile", "email" };
-            oktaMvcOptions.GetClaimsFromUserInfoEndpoint = true;
+		public void ConfigureServices(IServiceCollection services)
+		{
+			var oktaMvcOptions = new OktaMvcOptions();
+			Configuration.GetSection("Okta").Bind(oktaMvcOptions);
+			oktaMvcOptions.Scope = new List<string> { "openid", "profile", "email" };
+			oktaMvcOptions.GetClaimsFromUserInfoEndpoint = true;
 
-            services.AddAuthentication(options =>
-            {
-                options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-                options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-                options.DefaultChallengeScheme = OktaDefaults.MvcAuthenticationScheme;
-            })
-            .AddCookie()
-            .AddOktaMvc(oktaMvcOptions);
+			services.AddAuthentication(options =>
+			{
+				options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+				options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+				options.DefaultChallengeScheme = OktaDefaults.MvcAuthenticationScheme;
+			})
+			.AddCookie()
+			.AddOktaMvc(oktaMvcOptions);
 
-            services.AddMvc();
+			services.AddMvc();
+		}
 
-        }
+		public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+		{
+			if (env.IsDevelopment())
+			{
+				app.UseBrowserLink();
+				app.UseDeveloperExceptionPage();
+			}
+			else
+			{
+				app.UseExceptionHandler("/Home/Error");
+			}
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseBrowserLink();
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-            }
+			app.UseStaticFiles();
+			app.UseAuthentication();
 
-            app.UseStaticFiles();
-            app.UseAuthentication();
-            app.UseMvc(routes =>
-            {
-                routes.MapRoute(
-                    name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
-            });
-        }
-    }
+			app.UseMvc(routes =>
+			{
+				routes.MapRoute(
+					name: "default",
+					template: "{controller=Home}/{action=Index}/{id?}");
+			});
+		}
+	}
 }
