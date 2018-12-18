bb886f366e67642a46a92f2f80488ef75affdb99
Piotr Marczak <piotr.marczak@relativity.com>
2018-12-18 14:31:33 +0100

    Create report structure

--- okta-hosted-login/okta-aspnetcore-mvc-example/Models/ReportViewModel.cs
+++ okta-hosted-login/okta-aspnetcore-mvc-example/Models/ReportViewModel.cs
@@ -1,10 +1,11 @@
 ﻿using System;
+using TaxDeductionReporting.Models.Calendar;
 
 namespace TaxDeductionReporting.Models
 {
 	public class ReportViewModel
 	{
 		public DateTime Date { get; set; }
-
+		public CalendarViewModel Calendar { get; set; }
 	}
 }
