66d553e9db5b7793cc03173888364395aadbf670
Piotr Marczak <piotr.marczak@relativity.com>
2018-12-18 13:05:10 +0100

    Add bootstrap

--- okta-hosted-login/okta-aspnetcore-mvc-example/Startup.cs
+++ okta-hosted-login/okta-aspnetcore-mvc-example/Startup.cs
@@ -29,14 +29,14 @@ namespace okta_aspnetcore_mvc_example
             oktaMvcOptions.Scope = new List<string> { "openid", "profile", "email" };
             oktaMvcOptions.GetClaimsFromUserInfoEndpoint = true;
 
-            services.AddAuthentication(options =>
-            {
-                options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-                options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-                options.DefaultChallengeScheme = OktaDefaults.MvcAuthenticationScheme;
-            })
-            .AddCookie()
-            .AddOktaMvc(oktaMvcOptions);
+            //services.AddAuthentication(options =>
+            //{
+            //    options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+            //    options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+            //    options.DefaultChallengeScheme = OktaDefaults.MvcAuthenticationScheme;
+            //})
+            //.AddCookie()
+            //.AddOktaMvc(oktaMvcOptions);
 
             services.AddMvc();
 
